5d0de124b27b87f2d406e9fd7f46ff7d617fc947
Eleftherios Chaidemenos <l.haidemenos@gmail.com>
2023-04-05 15:29:19 +0300

    Update tests. Compare results

--- runTests.py
+++ runTests.py
@@ -1,38 +1,50 @@
 import os
+import filecmp
 
 # fileName, should pass
 tests = [
-  ['bsort.llama', True],
-  ['defThenUse.llama', True],
-  ['dynamicMem.llama', True],
-  ['evenOdd.llama', True],
-  ['hanoi.llama', True],
-  ['hello.llama', True],
-  ['letin.llama', True],
-  ['match.llama', False],                   # different return types in clauses
-  ['sameNames.llama', False],               # identifier re declaration
-  ['variableNotDeclared.llama', False],     #Unknown identifier: b
-  ['printInt.llama', True],
-  ['ifThenElse.llama', True],
-  ['forExpr.llama', True],
-  ['mutable.llama', True],
-  ['swap.llama', True],
-  ['assignment.llama', True],
-  ['dim.llama', True],
-  ['array.llama', True],
-  ['newDelete.llama', True],
-  ['2DArray.llama', True],
-  ['workingMatch.llama', True],
+  ['bsort', True],
+  ['defThenUse', True],
+  ['dynamicMem', True],
+  ['evenOdd', True],
+  ['hanoi', True],
+  ['hello', True],
+  ['letin', True],
+  ['match', False],                   # different return types in clauses
+  ['sameNames', False],               # identifier re declaration
+  ['variableNotDeclared', False],     # Unknown identifier: b
+  ['printInt', True],
+  ['ifThenElse', True],
+  ['forExpr', True],
+  ['mutable', True],
+  ['swap', True],
+  ['assignment', True],
+  ['dim', True],
+  ['array', True],
+  ['newDelete', True],
+  ['2DArray', True],
+  ['workingMatch', True],
 ]
 
 failed = 0
 
 for [test, shouldPass] in tests:
-  os.system(f"./Llama < ./example-programs/{test} > .test.temp 2>&1")
+  os.system(f"./do.sh ./example-programs/{test}.llama > .test.temp 2>&1")
   with open(".test.temp") as f:
     lines = f.readlines()
     if (not lines) != (shouldPass):
       failed = failed + 1
       print(f"{test}: failed with output: {lines}")
+      continue
+    if not shouldPass:
+      continue
+
+  os.system("scp a.out ubuntu16:~/.")
+  os.system(f"ssh ubuntu16 './a.out' > .test.temp 2>&1")
+  comp = filecmp.cmp('.test.temp', f"./example-programs/outputs/{test}")
+  if (not comp):
+    print(f"{test}: failed output mismatched")
+    failed = failed + 1
+ 
 
 print(f"Passed: {len(tests) - failed}. Failed: {failed}")
