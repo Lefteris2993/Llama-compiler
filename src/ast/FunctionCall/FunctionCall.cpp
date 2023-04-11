ad31944717d68255fa23306a20815538385430f9
Eleftherios Chaidemenos <l.haidemenos@gmail.com>
2023-04-11 21:26:29 +0300

    Update FunctionCall sem

--- src/ast/FunctionCall/FunctionCall.cpp
+++ src/ast/FunctionCall/FunctionCall.cpp
@@ -14,16 +14,29 @@ void FunctionCall::printOn(std::ostream &out) const {
 
 void FunctionCall::sem() {
   FunSymbolEntry *f = symbolTable->lookupEntry<FunSymbolEntry>(id, true, lineno);
-  if (f->entryType != EntryType::ENTRY_FUNCTION) 
+  if (f->entryType != EntryType::ENTRY_FUNCTION && f->entryType != EntryType::ENTRY_PARAMETER) 
     Logger::error(lineno, "\"%s\" is not a function", f->id.c_str());
 
-  if (f->paramNum != block->getBlockLength())
-    Logger::error(lineno, "Incorrect number of parameters passed in function :%s", id.c_str());
+  if (f->entryType == EntryType::ENTRY_PARAMETER) {
+    ParSymbolEntry *p = (ParSymbolEntry *)f;
+    if (p->type->getClassType() != TypeClassType::FUNCTION)
+      Logger::error(lineno, "\"%s\" is not a function", f->id.c_str());
+    
+    block->sem();
+    // TODO add parCheck here...
+    type = ((FunctionType *) p->type)->getRight();
+  }
 
-  block->sem();
-  block->parCheck(f);
+  if (f->entryType == EntryType::ENTRY_FUNCTION) {
+    if (f->paramNum != block->getBlockLength())
+      Logger::error(lineno, "Incorrect number of parameters passed in function :%s", id.c_str());
 
-  type = f->returnType;
+    block->sem();
+    block->parCheck(f);
+
+    type = f->returnType;
+  }
+  
 }
 
 llvm::Value* FunctionCall::codegen() {
